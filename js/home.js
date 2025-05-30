6b917c4018f9b83d5b5cb8b2d0bd3d6230ccbbe6
Sumaya <sheikhsumaya622@gmail.com>
2025-05-30 19:28:06 +0600

    toggle added

--- js/home.js
+++ js/home.js
@@ -62,3 +62,18 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
    }
 
 })
+
+// Cash out Button 
+
+// Show the cashout form 
+document.getElementById('btn-show-cash-out').addEventListener('click', function(){
+    document.getElementById('cash-out-form').classList.remove('hidden');
+    document.getElementById('add-money-form').classList.add('hidden');
+})
+
+// Show the addmoney form
+document.getElementById('btn-show-add-money').addEventListener('click', function(){
+    document.getElementById('add-money-form').classList.remove('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+})
+ 
